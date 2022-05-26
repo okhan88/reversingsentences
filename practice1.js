5acc4a75b94f00590df97ab580935f57d84c9481
osama <okpro333@gmail.com>
2022-05-26 14:19:07 -0400

    qs

---
+++ practice1.js
@@ -0,0 +1,11 @@
+// Let the input string be “i like this program very much”. The function should change the string to “much very program this like i”
+
+// Examples:
+// Input: s = “geeks quiz practice code” 
+// Output: s = “code practice quiz geeks”
+
+// Input: s = “getting good at coding needs a lot of practice” 
+// Output: s = “practice of lot a needs coding at good getting” 
+
+
+
