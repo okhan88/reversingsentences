83271bd95e41fd1ec0d9eaa0be7c3113a10e1fb3
osama <okpro333@gmail.com>
2022-05-28 23:39:20 -0400

    more notes

--- practice1.js
+++ practice1.js
@@ -7,5 +7,39 @@
 // Input: s = “getting good at coding needs a lot of practice” 
 // Output: s = “practice of lot a needs coding at good getting” 
 
+/*
+ step 1: let string1 = "i like this program very much"
 
 
+
+*/
+
+let fruits = ["banana", "cherry"]
+let fruitsstring = fruits.join(', ')
+console.log (fruitsstring)
+// output: "banana, cherry"
+
+let veges = ["carrot", "green"]
+let added = veges.push ("spinach")
+console.log (veges)
+// output: [ 'carrot', 'green', 'spinach' ]
+
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[1]);
+// expected output: "quick"
+
+const chars = str.split('');
+console.log(chars[0]);
+// expected output: "T"
+
+
+let vegetables = ['parsnip', 'potato']
+let moreVegs = ['celery', 'beetroot']
+
+// Merge the second array into the first one
+vegetables.push(...moreVegs);
+
+console.log(vegetables)  // ['parsnip', 'potato', 'celery', 'beetroot']
