73192d3caceea5b0db394526d20bb90d766e3783
osama <okpro333@gmail.com>
2022-05-30 12:54:25 -0400

    long method solve

--- practice1.js
+++ practice1.js
@@ -8,12 +8,44 @@
 // Output: s = “practice of lot a needs coding at good getting” 
 
 /*
- step 1: let string1 = "i like this program very much"
+ step 1: let string1 = ["i like this program very much"]
+ step 2: split the first word ("i") out of string1 and assign a new const
+ step 3: continue to split all the words in the string, assigning a new const for each one
+ step 4: push the first word of the solution string ("much") into string2
+ step 5: push the rest of the words in order into string2 in order to get desired string
+
 
 
 
 */
 
+
+// long method below
+
+const string1 = 'i like this program very much'
+const word1 = string1.split(' ');
+console.log (word1[0])
+console.log (word1[1])
+console.log (word1[2])
+console.log (word1[3])
+console.log (word1[4])
+console.log (word1[5])
+
+
+const list = []
+    let added = list.push(word1[5], word1[4], word1[3], word1[2], word1[1], word1[0])
+    console.log(list)
+
+
+
+
+// function reversingWords() 
+
+
+
+
+
+/*
 let fruits = ["banana", "cherry"]
 let fruitsstring = fruits.join(', ')
 console.log (fruitsstring)
@@ -43,3 +75,9 @@ let moreVegs = ['celery', 'beetroot']
 vegetables.push(...moreVegs);
 
 console.log(vegetables)  // ['parsnip', 'potato', 'celery', 'beetroot']
+
+
+
+
+
+*/
